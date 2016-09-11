a443389925e599067c8dbb970dfc29116926fa0d
amalkanth <amalkanth94@gmail.com>
2016-09-11 14:55:09 +0530

    admin logs in through this page

---
+++ admin_login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	admin login
+  </head>
+  <body>
+  <form action="admin_home.php" method="post">
+  <b>username:</b><input type="text" name="username">
+  
+   <b>password:</b><input type="password" name="password"> 
+   <input type="submit" value="login">
+  </form>
+  </body>
+  </html> 
