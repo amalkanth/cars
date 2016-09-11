eb19585f119e0f6e5909024909dd5377d7c931fe
amalkanth <amalkanth94@gmail.com>
2016-09-11 15:02:43 +0530

    this page adds new car

---
+++ add_new_car.php
@@ -0,0 +1,35 @@
+<html>
+<head>add a new car</head>
+<body>
+<form action="add_new_car.php" method="post"><br><br>
+	<b>ID:</b><input type="text" name="id"><br><br>
+	<b>Make:</b><input type="text" name="make"><br><br>
+	<b>Model:</b><input type="text" name="model"><br><br>
+	<b>Version:</b><input type="text" name="version"><br><br>
+	<b>Price Production Cars:</b><input type="text" name="price_prd"><br><br>
+    <b>Price discontinued cars(last recorded):</b><input type="text" name="price_old"><br><br>
+    <b>Length(mm):</b><input type="text" name="length"><br><br>
+    <b>Width(mm):</b><input type="text" name="width"><br><br>
+    <b>height(mm):</b><input type="text" name="height"><br><br>
+    <b>wheelbase(mm):</b><input type="text" name="wheelbase`"><br><br>
+    <b>ground clearence(mm):</b><input type="text" name="ground_clearence"><br><br>
+    <b>kerb weight(kg):</b><input type="text" name="kerb_weight"><br><br>
+    <b>bootspace(litres):</b><input type="text" name="bootspace"><br><br>
+    <b>No Of Doors:</b><input type="text" name="no_of_doors"><br><br>
+    <b>seating capacity:</b><input type="text" name="seating_capacity"><br><br>
+    <b>No Of Seating rows:</b><input type="text" name="no_of_seating_rows"><br><br>
+    <b>Displacment(cc):</b><input type="text" name="displacement"><br><br>
+    <b>Max Power(bhp)):</b><input type="text" name="max_power1"><br><br>
+    <b>Max power(rpm):</b><input type="text" name="max_power2"><br><br>
+    <b>Max Torque(Nm):</b><input type="text" name="max_torque1"><br><br>
+    <b>Max Torque(rpm):</b><input type="text" name="max_torque2"><br><br>
+    <b>Transmission type:</b><input type="text" name="transmission_type"><br><br>
+    <b>No Of Gears:</b><input type="text" name="no_of_gears"><br><br>
+    <b>Drive train:</b><input type="text" name="drivetrain"><br><br>
+    <b>Engine type:</b><input type="text" name="engine_type"><br><br>
+     <input type="submit" value="submit">
+ </form>
+
+</body>
+
+</html>
---
+++ admin_home.php
@@ -0,0 +1,8 @@
+<html>
+<head>admin home page</head><div>
+<body>
+ <input type="submit" name="to_do" value="ADD">
+  <input type="submit" name="to_do" value="EDIT">
+   <input type="submit" name="to_do" value="DELETE">
+ </body>
+ </html>
